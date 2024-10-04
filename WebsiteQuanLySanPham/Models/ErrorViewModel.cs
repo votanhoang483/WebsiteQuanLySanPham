5bebe61b1651355b40c9e44d6278450107fe7e9d
votanhoang483 <22520483@gm.uit.edu.vn>
2024-10-04 14:01:59 +0700

    init commit

---
+++ WebsiteQuanLySanPham/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebsiteQuanLySanPham.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
